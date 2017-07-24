a677b6f05685dc6967a37eb136fda06a1b90e2e7
Graham Ollis <plicease@cpan.org>
2017-07-24 10:11:20 -0400

    here is the actual implementation

--- src/main.c
+++ src/main.c
@@ -3,5 +3,15 @@
 int
 main(int argc, char *argv[])
 {
-  return 0;
+  /*
+   * If we do not have EXACTLY one argument,
+   * return a usage error.
+   */
+  if(argc != 2)
+    return 1;
+
+  if(is_palindrome(argv[1]))
+    return 0;
+  else
+    return 2;
 }
